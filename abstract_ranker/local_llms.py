a3710f7750bbce93770cab27afe669ffc4c7cefd
Gordon Watts <gwatts@uw.edu>
2024-06-07 21:31:58 +0200

    Make loading models more efficient

--- abstract_ranker/local_llms.py
+++ abstract_ranker/local_llms.py
@@ -1,5 +1,27 @@
 import logging
-from typing import Dict
+from typing import Any, Dict
+
+_hf_models: Dict[str, Any] = {}
+
+
+def create_pipeline(model_name: str):
+    """Create the pipeline for text generation using the specified model.
+
+    Args:
+        model_name (str): Name of the model to use.
+
+    Returns:
+        Pipeline: The text generation pipeline.
+    """
+
+    from transformers import pipeline
+
+    if model_name not in _hf_models:
+        _hf_models[model_name] = pipeline(
+            "text-generation", model=model_name, trust_remote_code=True
+        )
+
+    return _hf_models[model_name]
 
 
 def query_hugging_face(query: str, context: Dict[str, str], model_name: str) -> str:
@@ -18,8 +40,6 @@ def query_hugging_face(query: str, context: Dict[str, str], model_name: str) -> 
 Title: {context["title"]}
 Abstract: {context["abstract"]}"""
 
-    from transformers import pipeline
-
     messages = [
         {
             "role": "All of your answers will be parsed by a yaml parser. "
@@ -30,13 +50,7 @@ Abstract: {context["abstract"]}"""
     ]
     logger = logging.getLogger(__name__)
     logger.debug(f"Loading in model {model_name}")
-
-    # Create the pipeline
-    pipe = pipeline(
-        "text-generation",
-        model=model_name,
-        trust_remote_code=True,
-    )
+    pipe = create_pipeline(model_name)
 
     logger.debug("Running the pipeline")
     result = pipe(messages)
--- tests/test_local_llms.py
+++ tests/test_local_llms.py
@@ -45,3 +45,57 @@ Abstract: Abstract"""
             "(all output must be yaml). If the user gives you a template for the yaml, please "
             "use that (it will be in between ```yaml <template> ```."
         )
+
+
+def test_hf_twice():
+    "Make sure we do not create the same model twice"
+    with patch("transformers.pipeline") as mock_pipeline:
+        pipe_call_count = 0
+
+        def call_back(msg: str) -> str:
+            nonlocal pipe_call_count
+            pipe_call_count += 1
+            return "forking fork"
+
+        mock_pipeline.return_value = call_back
+
+        # Call twice!!
+        _ = query_hugging_face(
+            "What is the summary?",
+            {"title": "Title", "abstract": "Abstract"},
+            "microsoft/Phi-3-mini-4k-instruct",
+        )
+        _ = query_hugging_face(
+            "What is the summary?",
+            {"title": "Title", "abstract": "Abstract"},
+            "microsoft/Phi-3-mini-4k-instruct",
+        )
+        mock_pipeline.assert_called_once()
+        assert pipe_call_count == 2
+
+
+def test_hf_two_models():
+    "Make sure we do not create the same model twice"
+    with patch("transformers.pipeline") as mock_pipeline:
+        pipe_call_count = 0
+
+        def call_back(msg: str) -> str:
+            nonlocal pipe_call_count
+            pipe_call_count += 1
+            return "forking fork"
+
+        mock_pipeline.return_value = call_back
+
+        # Call twice!!
+        _ = query_hugging_face(
+            "What is the summary?",
+            {"title": "Title", "abstract": "Abstract"},
+            "microsoft/Phi-3-mini-4k-instruct",
+        )
+        _ = query_hugging_face(
+            "What is the summary?",
+            {"title": "Title", "abstract": "Abstract"},
+            "microsoft/Phi-3-mini-4k-instruct_t",
+        )
+        assert mock_pipeline.call_count == 2
+        assert pipe_call_count == 2
