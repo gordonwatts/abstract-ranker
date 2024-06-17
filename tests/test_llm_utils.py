16af10157f59b52f8259ca067d8ecd3abfb2339a
Gordon Watts <gwatts@uw.edu>
2024-06-17 15:11:11 -0700

    Fix up the LLM tests to work with dataclass.

--- tests/test_llm_utils.py
+++ tests/test_llm_utils.py
@@ -3,7 +3,15 @@ from unittest.mock import patch
 
 def test_dispatch():
     with patch("abstract_ranker.openai_utils.query_gpt") as mock_query_gpt:
-        mock_query_gpt.return_value = "Mocked response"
+        from abstract_ranker.llm_utils import AbstractLLMResponse
+
+        mock_query_gpt.return_value = AbstractLLMResponse(
+            summary="yes or no, you'll have to find out",
+            experiment="hi",
+            keywords=["hi"],
+            interest="high",
+            explanation="hi",
+        )
 
         context = {"title": "hi"}
         prompt = "hi"
@@ -11,7 +19,9 @@ def test_dispatch():
         from abstract_ranker.llm_utils import query_llm
 
         r = query_llm(prompt, context, "GPT4Turbo")
-        assert r == "Mocked response"
+        assert isinstance(r, AbstractLLMResponse)
+        assert r.interest == "high"
+        assert r.summary == "yes or no, you'll have to find out"
 
         assert mock_query_gpt.call_count == 1
         mock_query_gpt.assert_called_with(prompt, context, "gpt-4-turbo")
