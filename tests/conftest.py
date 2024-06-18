4988c80c9c35291566f33b6a906cb0ab6cb7f865
Gordon Watts <gwatts@uw.edu>
2024-06-18 19:47:04 -0400

    Trying to get the test working

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,6 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def setup_before_test():
-    "Reset state"
-    from abstract_ranker.local_llms import reset
-
-    reset()
-
-
-@pytest.fixture(autouse=True)
 def cache_dir(tmp_path):
     # Create a temporary test directory
     cache_dir = tmp_path / "cache_dir"
@@ -18,3 +10,11 @@ def cache_dir(tmp_path):
 
     with patch("abstract_ranker.config.CACHE_DIR", cache_dir):
         yield cache_dir
+
+
+@pytest.fixture()
+def setup_before_test():
+    "Reset state"
+    from abstract_ranker.local_llms import reset
+
+    reset()
--- tests/test_local_llms.py
+++ tests/test_local_llms.py
@@ -1,154 +1,91 @@
 from typing import Dict, List, Optional
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
-from abstract_ranker.local_llms import query_hugging_face
+import pytest
 
 
-def test_hf():
-    # Mock out the call to the transformers library pipeline call:
+@pytest.fixture()
+def pipeline_callback():
     with patch("abstract_ranker.local_llms.create_pipeline") as mock_pipeline:
-        call_message: Optional[List[Dict[str, str]]] = None
+        with patch(
+            "lmformatenforcer.integrations.transformers."
+            "build_transformers_prefix_allowed_tokens_fn"
+        ) as build_trans:
+            call_message: Optional[List[Dict[str, str]]] = None
 
-        def call_back(msg: List[Dict[str, str]], **kwargs) -> List[Dict[str, str]]:
-            nonlocal call_message
-            assert isinstance(msg, list)
-            assert len(msg) == 2
-            call_message = msg
-            return [{"generated_text": "test: forking fork"}]
+            class pipe_callback:
 
-        mock_pipeline.return_value = call_back
+                def __init__(self):
+                    self.tokenizer = MagicMock()
 
-        result = query_hugging_face(
-            "What is the summary?",
-            {"title": "Title", "abstract": "Abstract"},
-            "microsoft/Phi-3-mini-4k-instruct",
-        )
-        # Check the result
-        assert result == {"test": "forking fork"}
-        # Check the call
-        mock_pipeline.assert_called_once_with("microsoft/Phi-3-mini-4k-instruct")
+                def __call__(
+                    self, msg: List[Dict[str, str]], **kwargs
+                ) -> List[Dict[str, str]]:
+                    nonlocal call_message
+                    assert isinstance(msg, list)
+                    assert len(msg) == 2
+                    call_message = msg
+                    return [
+                        {
+                            "generated_text": '{"summary": "forking fork", "experiment": "", '
+                            '"keywords": [], "interest": "high", "explanation": "because"}'
+                        }
+                    ]
 
-        assert call_message is not None
-        for item in call_message:  # type: ignore
-            if item["role"] == "system":
-                assert item["content"].startswith("You are my expert")
-            elif item["role"] == "user":
-                assert (
-                    item["content"]
-                    == """What is the summary?
-Title: Title
-Abstract: Abstract"""
-                )
-            else:
-                assert False, f"Unknown role: {item['role']}"
+            mock_pipeline.return_value = pipe_callback()
+
+            build_trans.return_value = None
 
 
-def test_hf_md():
+# setup_before_test
+def test_hf(pipeline_callback):
     # Mock out the call to the transformers library pipeline call:
-    with patch("abstract_ranker.local_llms.create_pipeline") as mock_pipeline:
-        call_message: Optional[List[Dict[str, str]]] = None
+    from abstract_ranker.local_llms import query_hugging_face
 
-        def call_back(msg: List[Dict[str, str]], **kwargs) -> List[Dict[str, str]]:
-            nonlocal call_message
-            assert isinstance(msg, list)
-            assert len(msg) == 2
-            call_message = msg
-            return [
-                {
-                    "generated_text": " ```yaml\ntest: forking fork\n```\n\nThis is a bogus "
-                    "explanation it adds after the text"
-                }
-            ]
-
-        mock_pipeline.return_value = call_back
-
-        result = query_hugging_face(
-            "What is the summary?",
-            {"title": "Title", "abstract": "Abstract"},
-            "microsoft/Phi-3-mini-4k-instruct",
-        )
-        # Check the result
-        assert result == {"test": "forking fork"}
-        # Check the call
-        mock_pipeline.assert_called_once_with("microsoft/Phi-3-mini-4k-instruct")
-
-        assert call_message is not None
-        for item in call_message:  # type: ignore
-            if item["role"] == "system":
-                assert item["content"].startswith("You are my expert")
-            elif item["role"] == "user":
-                assert (
-                    item["content"]
-                    == """What is the summary?
-Title: Title
-Abstract: Abstract"""
-                )
-            else:
-                assert False, f"Unknown role: {item['role']}"
+    result = query_hugging_face(
+        "What is the summary?",
+        {"title": "Title", "abstract": "Abstract"},
+        "microsoft/Phi-3-mini-4k-instruct",
+    )
+    # Check the result
+    assert result.summary == "forking fork"
+    # Check the call
+    pipeline_callback.assert_called_once_with("microsoft/Phi-3-mini-4k-instruct")
 
 
-def test_hf_twice():
+def test_hf_twice(pipeline_callback):
     "Make sure we do not create the same model twice"
-    with patch("transformers.AutoModelForCausalLM.from_pretrained") as auto_causal_mock:
-        with patch("transformers.AutoTokenizer.from_pretrained") as auto_token_mock:
-            with patch("transformers.pipeline") as pipeline_mock:
+    # Call twice!!
+    from abstract_ranker.local_llms import query_hugging_face
 
-                auto_causal_mock.return_value = "model"
-                auto_token_mock.return_value = "tokenizer"
+    _ = query_hugging_face(
+        "What is the summary?",
+        {"title": "Title", "abstract": "Abstract"},
+        "microsoft/Phi-3-mini-4k-instruct",
+    )
+    _ = query_hugging_face(
+        "What is the summary?",
+        {"title": "Title", "abstract": "Abstract"},
+        "microsoft/Phi-3-mini-4k-instruct",
+    )
 
-                pipe_call_count = 0
-
-                def call_back(msg: str, **_) -> List[Dict[str, str]]:
-                    nonlocal pipe_call_count
-                    pipe_call_count += 1
-                    return [{"generated_text": "forking fork"}]
-
-                pipeline_mock.return_value = call_back
-
-                # Call twice!!
-                _ = query_hugging_face(
-                    "What is the summary?",
-                    {"title": "Title", "abstract": "Abstract"},
-                    "microsoft/Phi-3-mini-4k-instruct",
-                )
-                _ = query_hugging_face(
-                    "What is the summary?",
-                    {"title": "Title", "abstract": "Abstract"},
-                    "microsoft/Phi-3-mini-4k-instruct",
-                )
-                pipeline_mock.assert_called_once()
-                assert pipe_call_count == 2
+    pipeline_callback.assert_called_once()
 
 
-def test_hf_two_models():
+def test_hf_two_models(pipeline_callback):
     "Make sure we create different models"
-    with patch("transformers.AutoModelForCausalLM.from_pretrained") as auto_causal_mock:
-        with patch("transformers.AutoTokenizer.from_pretrained") as auto_token_mock:
-            with patch("transformers.pipeline") as pipeline_mock:
+    from abstract_ranker.local_llms import query_hugging_face
 
-                auto_causal_mock.return_value = "model"
-                auto_token_mock.return_value = "tokenizer"
+    # Call twice with different model names
+    _ = query_hugging_face(
+        "What is the summary?",
+        {"title": "Title", "abstract": "Abstract"},
+        "microsoft/Phi-3-mini-4k-instruct",
+    )
+    _ = query_hugging_face(
+        "What is the summary?",
+        {"title": "Title", "abstract": "Abstract"},
+        "microsoft/Phi-3-mini-4k-instruct_t",
+    )
 
-                pipe_call_count = 0
-
-                def call_back(msg: str, **_) -> List[Dict[str, str]]:
-                    nonlocal pipe_call_count
-                    pipe_call_count += 1
-                    return [{"generated_text": "forking fork"}]
-
-                pipeline_mock.return_value = call_back
-
-                # Call twice with different model names
-                _ = query_hugging_face(
-                    "What is the summary?",
-                    {"title": "Title", "abstract": "Abstract"},
-                    "microsoft/Phi-3-mini-4k-instruct",
-                )
-                _ = query_hugging_face(
-                    "What is the summary?",
-                    {"title": "Title", "abstract": "Abstract"},
-                    "microsoft/Phi-3-mini-4k-instruct_t",
-                )
-
-        assert pipeline_mock.call_count == 2
-        assert pipe_call_count == 2
+    assert pipeline_callback.call_count == 2
