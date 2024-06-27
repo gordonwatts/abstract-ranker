014a89a110c73bd862b71cc602c25c7a5cd946db
Gordon Watts <gwatts@uw.edu>
2024-06-27 15:47:55 -0400

    Get the cache test to work properly

--- tests/test_llm_utils.py
+++ tests/test_llm_utils.py
@@ -25,3 +25,29 @@ def test_dispatch():
 
         assert mock_query_gpt.call_count == 1
         mock_query_gpt.assert_called_with(prompt, context, "gpt-4-turbo")
+
+
+def test_cache(cache_dir):
+    "Make sure that caching is used if called twice"
+
+    # with patch("abstract_ranker.openai_utils.query_gpt") as mock_query_gpt:
+    with patch("abstract_ranker.llm_utils.query_gpt") as mock_query_gpt:
+        from abstract_ranker.llm_utils import AbstractLLMResponse
+
+        mock_query_gpt.return_value = AbstractLLMResponse(
+            summary="yes or no, you'll have to find out",
+            experiment="hi",
+            keywords=["hi"],
+            interest="high",
+            explanation="hi",
+        )
+
+        context = {"title": "hi-unique-test"}
+        prompt = "hi-fork-one"
+
+        from abstract_ranker.llm_utils import query_llm
+
+        query_llm(prompt, context, "GPT4Turbo")
+        query_llm(prompt, context, "GPT4Turbo")
+
+        assert mock_query_gpt.call_count == 1
