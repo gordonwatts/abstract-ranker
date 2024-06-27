32264eaa25a613a118ef68af9bfb1a93a4fe04cb
Gordon Watts <gwatts@uw.edu>
2024-06-27 16:14:58 -0400

    Include hints that we might need later to tune

--- abstract_ranker/data_model.py
+++ abstract_ranker/data_model.py
@@ -30,3 +30,14 @@ class AbstractLLMResponse(BaseModel):
         ...,
         title="Explanation of the interest level in the abstract. Very short.",
     )
+
+
+# Please format your with a summary  (One line, terse, summary of the abstract that
+# does not repeat the title. It should add extra information beyond the title, and should mention
+# any key outcomes that are present in the abstract), an experiment name (If you can guess the
+# experiment this abstract is associated with (e.g. ATLAS, CMS, LHCb, etc), place it here.
+# Otherwise
+# leave it blank), a list of keywords (json-list of 4 or less keywords or phrases describing topics
+# in the below abstract and title, comma separated, pulled from my list of interests), and my
+# expected interest(put: "high" (hits several of the interests listed above), "medium" (hits one
+# interest), or "low" (hits a not interest). Be harsh, my time is valuable).
