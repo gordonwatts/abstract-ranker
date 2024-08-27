d9d7a59d2f63ca923a8308c6047c364fff7f3139
Gordon Watts <gwatts@uw.edu>
2024-08-27 14:19:35 -0700

    Deal with latex text coming back

--- tests/test_openai_utils.py
+++ tests/test_openai_utils.py
@@ -135,3 +135,48 @@ def test_openai_bad_response():
                 )
 
             assert "validation error" in str(e.value)
+
+
+def test_openai_latex_response():
+    with patch("openai.OpenAI") as mock_openai:
+        with patch("abstract_ranker.openai_utils.get_key") as mock_get_key:
+            mock_get_key.return_value = "bogus_key"
+
+            mock_openai.return_value.chat.completions.create.return_value = response(
+                choices=[
+                    choice(
+                        message=message(
+                            content="""{
+    "summary": "Not given",
+    "experiment": "",
+    "keywords": ["b to s transitions", "flavor physics", "lepton interactions"],
+    "interest": "low",
+    "explanation": "The topic of $b \\to s \\ell \\ell$ fits does not align with my interests \
+in hidden sector physics or long-lived particles.",
+    "confidence": 0.3,
+    "unknown_terms": []
+}
+"""
+                        )
+                    )
+                ]
+            )
+
+            from abstract_ranker.openai_utils import query_gpt
+
+            r = query_gpt(
+                "hi",
+                {
+                    "title": "hi",
+                    "abstract": "hi",
+                    "interested_topics": ["hi", "there"],
+                    "not_interested_topics": ["no", "thanks"],
+                },
+                "gpt-4-turbo-bogus",
+            )
+
+            assert (
+                r.explanation
+                == "The topic of $b \\to s \\ell \\ell$ fits does not align with my interests "
+                "in hidden sector physics or long-lived particles."
+            )
